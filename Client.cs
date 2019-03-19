4b04f1759a99863e48c5669784a7123c4816a051
NEzweitausend <48239361+NEzweitausen@users.noreply.github.com>
2019-03-19 08:12:00 +0100

    19.03.

--- Client.cs
+++ Client.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,16 +10,64 @@ namespace RPS
 {
     class Client
     {
+        private string eingabeTCP = "TEST ÜBERMITTLUNG";
+        private string ausgabeTCP;
+        private string Servername;
+        private int ServerPort = 4712;
+        private bool running;
+
+        private Eingabe e = new Eingabe();
+
         public Client() //Konstruktor
         {
-            Console.WriteLine("Test Konstruktor Client");
-            Console.ReadKey();
+ 
+            Console.WriteLine("Die TCP-Verbindung muss aufgebaut werden.\nGeben Sie den Hostnamen ein und bestätigen Sie mit <Enter>.");
+            Servername = e.EingabeString();
+            
+            bool erfolgreich = TCPverbindung();
+
+            if (erfolgreich == false) return;
         }
 
-        //Mehrspielermodus TODO
-        private void TCPverbindung()
+
+        private bool TCPverbindung()
         {
 
+            
+            TcpClient client = new TcpClient();
+
+            try
+            {
+                client.Connect(Servername, ServerPort);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Fehler bei TCP-Verbindung ! Port:  " + ServerPort);
+                return false;
+            }
+            catch
+            {
+                Console.WriteLine("Fehler im Verbindungsprozess - nicht TCP an sich.");
+            }
+
+
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+            writer.AutoFlush = true;
+
+            while (running == true)
+            {
+                writer.WriteLine(eingabeTCP);
+                Console.WriteLine("Anfrage gesendet.");
+
+                ausgabeTCP = reader.ReadLine();
+
+                Console.WriteLine(ausgabeTCP);
+                if (ausgabeTCP == "EXIT") running = false;
+            }
+
+            Console.ReadKey();
+            return true;
         }
     }
 }
